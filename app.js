f4fc0866bbb821b517e5cd41ed975540c11241a7
nelkeedo <ndolo2965@gmail.com>
2024-05-17 11:17:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+console.log("welcome to my game ")
+/*const playmode= require("playmode")
+console.log("press 1 to start game and any other key to exit ")
+function start(){
+    if(playmode==1){
+        console.log("you have started the game ")
+       playgame
+    }else{
+        console.log("thanky you for your time ")
+    }
+
+}*/
+var random= math.floor(math.random(20) +1)
+ console.log(random)
